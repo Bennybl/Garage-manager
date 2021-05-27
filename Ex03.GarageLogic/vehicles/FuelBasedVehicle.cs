f181aa93315db2c4b1615e4d12833ce793f98ce6
Benny Blaustein <benny.blaustein@post.idc.ac.il>
2021-05-27 18:38:46 +0300

    comm

--- Ex03.GarageLogic/vehicles/FuelBasedVehicle.cs
+++ Ex03.GarageLogic/vehicles/FuelBasedVehicle.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace Ex03.GarageLogic.vehicles
 {
-    class FuelBasedVehicle
+    class FuelBasedVehicle : Vehicle
     {
+        protected eFuelType m_FuelType;
+        protected float m_CurrentFuel;
+        protected const float k_= 0;
     }
 }
--- Ex03.GarageLogic/vehicles/Vehicle.cs
+++ Ex03.GarageLogic/vehicles/Vehicle.cs
@@ -6,5 +6,10 @@ namespace Ex03.GarageLogic.vehicles
 {
     class Vehicle
     {
+        protected string m_LicenceNumber;
+        protected float m_energyLeft;
+        protected Tire[] m_VehicleTire;
+        protected string m_ModelName;
+        ///protected const float k_MinimumValue=0;
     }
 }
