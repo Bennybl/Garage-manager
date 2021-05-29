53abc86b5e7d6ed3e32b5a79053b793e9d3e06e2
nirohub <nir1201@gmail.com>
2021-05-29 07:00:35 +0300

    v1.1

--- Ex03.ConsoleUI/UI.cs
+++ Ex03.ConsoleUI/UI.cs
@@ -6,5 +6,15 @@ namespace Ex03.ConsoleUI
 {
     class UI
     {
+        private enum eUserChoice
+        {
+            InsertNewVehicle = 1,
+            DisplayVehicles = 2,
+            UpdateVehicleStatus = 3,
+            InflateVehicleTiresToMaximum = 4,
+            FuelVehicle = 5,
+            ChargeElectricVehicle = 6,
+            DisplayVehicleInformation = 7
+        }
     }
 }
