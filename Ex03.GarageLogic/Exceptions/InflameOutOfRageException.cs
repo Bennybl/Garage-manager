fc8106430eefbb50b994b9b58c710c9781d0c612
Benny's xps <benny.blaustein@post.idc.ac.il>
2021-05-30 21:55:17 +0300

    commit

---
+++ Ex03.GarageLogic/Exceptions/InflameOutOfRageException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class InflameOutOfRageException : Exception
+    {
+        public InflameOutOfRageException(float i_MaxAirPressure) :
+            base(string.Format("In flame out of rage exception! Max air pressure: {0}", i_MaxAirPressure))
+        { 
+        }
+    }
+}
